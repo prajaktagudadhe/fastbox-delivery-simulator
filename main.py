e71b1bb77909b8a70e87b58c19ac4ff2ce8160f7
Prajakta Gudadhe <prajaktagudadhe@outlook.com>
2026-05-27 19:46:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
+import json
+from simulator import (
+    simulate,
+    top_performer,
+)
 
+FILE = "base_case.json"
+
+def main():
+    with open(FILE) as f:
+        data = json.load(f)
+    (
+        report,
+        totals,
+        delivered_count
+    ) = simulate(data)
+    print(
+        "\nDELIVERY REPORT"
+    )
+    for item in report:
+        print(item)
+    print(
+        "\nTOTAL DISTANCE"
+    )
+    for agent, d in totals.items():
+        print(
+            f"{agent}: {round(d,2)}"
+        )
+    best = top_performer(
+        totals,
+        delivered_count
+    )
+    print(
+        "\nTOP PERFORMER:",
+        best
+    )
+if __name__ == "__main__":
+    main()
