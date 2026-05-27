8d28ac88f4f1dfc4bc3a9641395d1705c798425b
Prajakta Gudadhe <prajaktagudadhe@outlook.com>
2026-05-27 19:42:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,40 @@
+import math
+def distance(p1, p2):
+    return math.sqrt(
+        (p1[0] - p2[0]) ** 2 +
+        (p1[1] - p2[1]) ** 2
+    )
+  
+def normalize_warehouses(data):
+    raw = data["warehouses"]
+    if isinstance(raw, list):
+        return {
+            w["id"]: w["location"]
+            for w in raw
+        }
+    return raw
 
+def normalize_agents(data):
+    raw = data["agents"]
+    if isinstance(raw, list):
+        return {
+            a["id"]: a["location"]
+            for a in raw
+        }
+    return raw
+  
+def normalize_packages(data):
+    packages = []
+    for p in data["packages"]:
+        warehouse = (
+            p.get("warehouse_id")
+            or p.get("warehouse")
+        )
+
+        packages.append({
+            "id": p["id"],
+            "warehouse": warehouse,
+            "destination": p["destination"]
+        })
+
+    return packages
